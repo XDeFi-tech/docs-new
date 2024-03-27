5c1611256661d0e5701e116757d14d13d6f3625f
Justin <102846781+HoangVD2@users.noreply.github.com>
2024-03-27 19:06:58 +0700

    docs: add documentation for Assets and Prices API section (#22)

---
+++ components/AssetsFiatCurrencies.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import LoadingIcon from "./LoadingIcon";
+import PlayIcon from "./PlayIcon";
+
+const FiatCurrencies = () => {
+  const GRAPHQL_ENDPOINT = "https://gql-router.xdefiservices.com/graphql";
+  const [response, setResponse] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const query = `
+  query FiatCurrencies($page: ConnectionArgs!, $filter: FiatCurrencyFilter, $after: DateTime, $afterPrice: DateTime) {
+    assets {
+      fiatCurrencies(page: $page, filter: $filter, after: $after, afterPrice: $afterPrice) {
+        pageData {
+          count
+          limit
+          offset
+        }
+        page {
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
+          edges {
+            node {
+              id
+              name
+              symbol
+              scalingFactor
+              character
+              price {
+                amount
+              }
+            }
+          }
+        }
+      }
+    }
+  }`;
+
+  const vars = {
+    page: {
+      first: 5,
+      after: null,
+    },
+    filter: {
+      ids: null,
+    },
+    after: null,
+    afterPrice: null,
+  };
+
+  const getFiatCurrencies = async () => {
+    setLoading(true);
+    setResponse({});
+
+    await fetch(GRAPHQL_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query,
+        variables: vars,
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        setResponse(result);
+      })
+      .catch((error) => {
+        setResponse(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <>
+      <div className="flex justify-center">
+        <button
+          onClick={getFiatCurrencies}
+          className="flex justify-center items-center gap-2 bg-[#2770CB] text-white px-2 py-1 rounded"
+          disabled={loading}
+        >
+          {loading ? (
+            <>
+              <LoadingIcon />
+              Fetching...
+            </>
+          ) : (
+            <>
+              <PlayIcon />
+              Test the query
+            </>
+          )}
+        </button>
+      </div>
+      <div className="my-4 rounded-lg max-h-[600px] overflow-auto bg-[#F6F6F7] text-[#24292E] dark:bg-[#161618] dark:text-[#E1E4E8]">
+        <div className="px-5 border-b border-[#e2e2e3] dark:border-black">
+          <span className="inline-block border-b-2 border-[#3451b2] dark:border-[#a8b1ff] text-[14px] leading-[48px]">
+            Response
+          </span>
+        </div>
+        <pre className="p-5">{JSON.stringify(response, null, 2)}</pre>
+      </div>
+    </>
+  );
+};
+
+export default FiatCurrencies;
---
+++ components/SubscriptionService.jsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import LoadingIcon from "./LoadingIcon";
+import PlayIcon from "./PlayIcon";
+import { gql, ApolloClient, InMemoryCache } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+
+const SubscriptionServices = () => {
+  const [response, setResponse] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const wsLink = new GraphQLWsLink(
+    createClient({
+      url: "wss://subscription-service.dev.xdefi.services/graphql",
+    }),
+  );
+
+  const query = gql`
+    subscription Subscription($ids: [String!]) {
+      price(ids: $ids) {
+        chain
+        id
+        price {
+          amount
+        }
+        symbol
+        type
+        name
+        contracts {
+          address
+          chain
+          symbol
+          scalingFactor
+          defiProtocol {
+            icon
+            chain
+            name
+          }
+          id
+        }
+        icon
+        externalData
+        marketCap
+        scalingFactor
+      }
+    }
+  `;
+
+  const vars = {
+    ids: [],
+  };
+
+  const subscriptionServices = async () => {
+    if (loading) {
+      setLoading(false);
+      return;
+    }
+    setLoading(true);
+    setResponse({});
+
+    const client = new ApolloClient({
+      link: wsLink,
+      cache: new InMemoryCache(),
+    });
+
+    client
+      .subscribe({
+        query,
+        variables: vars,
+      })
+      .subscribe({
+        next: (data) => {
+          setResponse(data);
+        },
+        error: (error) => {
+          console.error(error);
+        },
+        complete: () => {
+          console.log("completed");
+        },
+      });
+  };
+
+  return (
+    <>
+      <div className="flex justify-center">
+        <button
+          onClick={subscriptionServices}
+          className="flex justify-center items-center gap-2 bg-[#2770CB] text-white px-2 py-1 rounded"
+        >
+          {loading ? (
+            <>
+              <LoadingIcon />
+              Listening...
+            </>
+          ) : (
+            <>
+              <PlayIcon />
+              Subscription
+            </>
+          )}
+        </button>
+      </div>
+      <div className="my-4 rounded-lg max-h-[600px] overflow-auto bg-[#F6F6F7] text-[#24292E] dark:bg-[#161618] dark:text-[#E1E4E8]">
+        <div className="px-5 border-b border-[#e2e2e3] dark:border-black">
+          <span className="inline-block border-b-2 border-[#3451b2] dark:border-[#a8b1ff] text-[14px] leading-[48px]">
+            Response
+          </span>
+        </div>
+        <pre className="p-5">{JSON.stringify(response, null, 2)}</pre>
+      </div>
+    </>
+  );
+};
+
+export default SubscriptionServices;
